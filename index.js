4f2389e0560ebd109b64dc504306caf875695b11
Bikram Sodhi <biksodhi@gmail.com>
2020-01-23 18:03:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var recipes = {}
+
 function updateOnjectWithKeyAndValue(object, key, value){
   return object.assign({}, obj,{[key], value})
 }
-const recipe = {lemon:4}
 
 updateOnjectWithKeyAndValue(recipe, "chocolate": "0 cups")
