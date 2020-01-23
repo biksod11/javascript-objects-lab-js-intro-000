f1b458c68678ef219f26dc8d9a4bcc4b2f867b29
Bikram Sodhi <biksodhi@gmail.com>
2020-01-23 18:04:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign({}, object,{[key], value})
+  return object.assign({}, object,{[key]:value})
 }
 
 updateObjectWithKeyAndValue(recipe, "chocolate": "0 cups")
